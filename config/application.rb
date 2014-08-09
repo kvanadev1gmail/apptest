d7b2e9b5a4b57ae28f5272a24002ee85c8eea9ce
kvanadev <kvanadev1@gmail.com>
2014-08-09 18:30:25 +0530

    logins

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -19,5 +20,7 @@ module Apptest
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.initialize_on_precompile = false
   end
 end
